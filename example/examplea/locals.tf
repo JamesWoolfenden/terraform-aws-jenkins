c2524fb4c3a2c6f3662bf9dcfcdcfa1dc15727c1
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:37:03 +0100

    fix: update terraform-http-ip to response_body fix commit

--- example/examplea/locals.tf
+++ example/examplea/locals.tf
@@ -1,8 +1,6 @@
 module "ip" {
-  source  = "JamesWoolfenden/ip/http"
-  version = "0.3.7"
+  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=5769331633debca683a81a38470083a0abd39049"
 }
-
 locals {
   ip = module.ip.cidr
 }
