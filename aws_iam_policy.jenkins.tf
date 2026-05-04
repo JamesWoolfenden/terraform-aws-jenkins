d857dd6bcd1c51003e360140ca072610f9fe7e3a
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:28:44 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- aws_iam_policy.jenkins.tf
+++ aws_iam_policy.jenkins.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_policy" "jenkins" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   name        = var.jenkins_name
   description = "Allow Management account to STS to other accounts"
 
